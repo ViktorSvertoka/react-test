51dc0817fe3416694895b276f742a9db9371363e
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-11-25 19:27:54 +0200

    ⚠️ Fix(HTML & JSX) Change code

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,33 +1,36 @@
-import { NavLink, Outlet } from "react-router-dom";
-import { Suspense } from "react";
+import { NavLink, Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
 
 export default function Layout() {
   return (
     <>
-      <NavLink
-        to="/catalog"
-        className={({ isActive, isPending }) =>
-          isPending ? "pending" : isActive ? "active" : ""
-        }
-      >
-        Catalog
-      </NavLink>
-      <NavLink
-        to="/"
-        className={({ isActive, isPending }) =>
-          isPending ? "pending" : isActive ? "active" : ""
-        }
-      >
-        Home
-      </NavLink>
-      <NavLink
-        to="/about"
-        className={({ isActive, isPending }) =>
-          isPending ? "pending" : isActive ? "active" : ""
-        }
-      >
-        About
-      </NavLink>
+      <header style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+        <NavLink
+          to="/catalog"
+          className={({ isActive, isPending }) =>
+            isPending ? 'pending' : isActive ? 'active' : ''
+          }
+        >
+          Catalog
+        </NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive, isPending }) =>
+            isPending ? 'pending' : isActive ? 'active' : ''
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive, isPending }) =>
+            isPending ? 'pending' : isActive ? 'active' : ''
+          }
+        >
+          About
+        </NavLink>
+      </header>
+
       <Suspense>
         <Outlet />
       </Suspense>
