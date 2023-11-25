851059b2ee3143ddf89a476b7cb5e5426ec38130
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-11-25 19:51:31 +0200

    ⚠️ Fix(HTML & JSX) Change code

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Catalog from "./pages/Catalog";
-import About from "./pages/About";
-import Layout from "./Layout/Layout";
+import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import About from './pages/About';
+import Layout from './Layout/Layout';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Home />} />
-      </Route>
-    </Routes>
-  );
+  // return (
+  //   <Routes>
+  //     <Route path="/" element={<Layout />}>
+  //       <Route index element={<Home />} />
+  //       <Route path="/catalog" element={<Catalog />} />
+  //       <Route path="/about" element={<About />} />
+  //       <Route path="*" element={<Home />} />
+  //     </Route>
+  //   </Routes>
+  // );
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          element: <Home />,
+          index: true,
+        },
+        {
+          path: '/catalog',
+          element: <Catalog />,
+        },
+        {
+          path: '/about',
+          element: <About />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
